94f953a86cf98a090d6ba0cd59d457dfe313ea19
Rudy Hartwig <rudyhartwig25@outlook.com>
2026-01-04 16:01:07 -0500

    chore: added common import aliases

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,18 +1,35 @@
-import { resolve } from 'path'
-import { defineConfig } from 'electron-vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import svgr from 'vite-plugin-svgr'
+import { resolve } from "path";
+import { defineConfig } from "electron-vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
-  main: {},
-  preload: {},
+  main: {
+    resolve: {
+      alias: {
+        "@": resolve("src"),
+        "@api": resolve("src/api")
+      }
+    }
+  },
+  preload: {
+    resolve: {
+      alias: {
+        "@": resolve("src"),
+        "@api": resolve("src/api")
+      }
+    }
+  },
   renderer: {
     resolve: {
       alias: {
-        '@renderer': resolve('src/renderer/src')
+        "@": resolve("src"),
+        "@renderer": resolve("src/renderer/src"),
+        "@assets": resolve("src/renderer/src/assets"),
+        "@api": resolve("src/api")
       }
     },
     plugins: [react(), tailwindcss(), svgr()]
   }
-})
+});
